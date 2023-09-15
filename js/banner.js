fa48c2da665433486f2c92911b63733143dc74d4
Elizabeth Kodjo <ellizakodjo@outlook.com>
2023-09-15 10:56:33 +0100

    Banner slider is working. Started on mobile menu

--- js/banner.js
+++ js/banner.js
@@ -22,3 +22,16 @@ function showSlides() {
     setTimeout(showSlides, 3000);
 }
 
+
+
+// Mobile nav
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft ="0";
+}
