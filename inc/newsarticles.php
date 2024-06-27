99399a043294abcb7786906ba151346ffe49ddd6
Elizabeth Kodjo <ellizakodjo@outlook.com>
2024-06-27 05:59:11 +0100

    Changed the newsarticles colors

--- inc/newsarticles.php
+++ inc/newsarticles.php
@@ -18,52 +18,7 @@
         }
     }
    
-//     try {  
-       
-//         require_once "dbconnect.php";
-//         //$db = connect(); 
-      
-        
-                       
 
-//         $newsQuery = $db->query("SELECT * FROM articles ORDER BY date DESC LIMIT 3");
-//         //$newsQuery->execute();
-//         //$record = $newsQuery->fetch(PDO::FETCH_ASSOC);
-//         //print_r($record);
-
-//         $records = $newsQuery->fetchAll(PDO::FETCH_ASSOC);
-//         //print_r($records);
-//         echo "Connected to database<br>";
-       
-//             // foreach ($records as $test) {
-//             //      //print_r($test['id'] . "<br>");
-//             //     //echo $records;
-//             //     $id = $test['id'];
-//             //     $image = $test['image'];
-//             //     $blocktext = $test['blocktext'];
-//             //     $readtime = $test['readtime'];
-//             //     $paragraph = $test['paragraph'];
-//             //     $avatar = $test['avatar'];
-//             //     $avataralt = $test['avataralt'];
-//             //     $postedby = $test['postedby'];
-//             //     $date = $test['date'];
-               
-          
-                
-//             //     echo $id;
-//             //     //print_r($records["id"]);
-//             //     $testid = $test;
-//             //     //print_r($info);
-//             //     //print($test['image']);
-//             //     //print_r($test['id']);
-//             }
-        
-   
-//     } catch (PDOException $e) {
-//         echo "Tesing failed: " . $e->getMessage();
-//         exit;
-//     }
-// //}
 
 
 
@@ -78,20 +33,20 @@ function newsBlock($id, $image, $alt, $blocktext, $heading, $paragraph, $readtim
                 
                     <div><img class="jobvac" src="'. $image .'" alt="'.htmlspecialchars($alt).'">
                     </div>
-                    <div class="text-block1">
-                        <a href="#">'.htmlspecialchars($blocktext).'</a>
+                    <div class="text-block" style="background-color: ' .htmlspecialchars($color) . ';">
+                        <a href="#" style="background-color: ' . htmlspecialchars($color) . ';">'.htmlspecialchars($blocktext).'</a>
                     </div>
                 </div>
                 <div class="container">
                     <div class="art-details">
                         
-                        <h3 class="jobtitle'.htmlspecialchars($id).'">'.htmlspecialchars($heading).'<span class="dots">...</span><span class="jobspan">
+                        <h3 class="jobtitle" style="color: ' .htmlspecialchars($color) . '">'.htmlspecialchars($heading).'<span class="dots">...</span><span class="jobspan">
                                     '. htmlspecialchars($readtime) .'</span></h3>
                         <div class="jobspec">
                             <p>' . htmlspecialchars($paragraph) . '<span class="dots">...</span>
                             </p>
                         </div>
-                        <button class="btn latestbtn1" background-color{color:'. htmlspecialchars($color) .'; } >Read more</button><br>
+                        <button class="btn latestbtn1" style:"background-color:' . htmlspecialchars($color) .';"  >Read more</button><br>
                         <div class="dateposted">
                             <img class="jobpic" src="' . htmlspecialchars($avatar) .'" alt="' . htmlspecialchars($avataralt) .'">
                             <div class="postedby">
